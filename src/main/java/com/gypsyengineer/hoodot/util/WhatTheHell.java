51bd400033a3985d7cfa4612e603cfdd65251af6
Artem Smotrakov <artem.smotrakov@gmail.com>
2018-10-27 22:39:59 +0200

    Added WhatTheHell

---
+++ src/main/java/com/gypsyengineer/hoodot/util/WhatTheHell.java
@@ -0,0 +1,23 @@
+package com.gypsyengineer.hoodot.util;
+
+/**
+ * Your favorite exception.
+ */
+public class WhatTheHell extends RuntimeException {
+
+    public static WhatTheHell whatTheHell(String message) {
+        return new WhatTheHell(message);
+    }
+
+    public static WhatTheHell whatTheHell(String message, Throwable t) {
+        return new WhatTheHell(message, t);
+    }
+
+    private WhatTheHell(String message) {
+        super(message);
+    }
+
+    private WhatTheHell(String message, Throwable t) {
+        super(message, t);
+    }
+}
